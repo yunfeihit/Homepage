29a84377b77b143d839dc12b7d135f7333de33f1
yunfeihit <yunfeihit@gmail.com>
2026-06-30 17:04:45 +0800

    submit version 1.1

--- src/scale-iframe.js
+++ src/scale-iframe.js
@@ -2,14 +2,21 @@ const iframes = document.querySelectorAll('iframe');
 
 function scaleIframes() {
     iframes.forEach(iframe => {
-        const originlWitdh = iframe.offsetWidth;
-        const originlHeight = iframe.offsetHeight;
-        const scaleRatio = originlWitdh / 1200;
+        const originalWitdh = iframe.offsetWidth;
+        const scaleRatio = originalWitdh / 1200;
 
         iframe.style.width = "1200px";
-        iframe.style.height = originlHeight / scaleRatio;
         iframe.style.transform = `scale(${scaleRatio})`;
         iframe.style.transformOrigin = "top left";
+
+        try {
+            const contentHeight = iframe.contentWindow.document.body.scrollHeight;
+            iframe.style.height = contentHeight + "px";
+            const wrapper = iframe.parentElement;
+            wrapper.style.height = (contentHeight * scaleRatio) + "px";
+        } catch(e) {
+            console.warn('Cannot read iframe content (likely cross-origin):', e);
+        }
     })
 }
 
